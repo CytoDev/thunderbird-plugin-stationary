7cca6c7a2cc83691ad6e66442d9d0b21960880eb
Roel Walraven <mail@cytodev.io>
2019-11-29 16:15:14 +0100

    Reformat js files

--- src/chrome/content/stationery.js
+++ src/chrome/content/stationery.js
@@ -1,10 +1,11 @@
 /******************************************************************************
-project: "Stationery" extension for Thunderbird
-filename: stationery.jsm
-author: Łukasz 'Arivald' Płomiński <arivald@interia.pl>
+ project: "Stationery" extension for Thunderbird
+ filename: stationery.jsm
+ author: Łukasz 'Arivald' Płomiński <arivald@interia.pl>
 
-description: Stationery module importer
-******************************************************************************/
+ description: Stationery module importer
+ ******************************************************************************/
 
-Components.utils.import('resource://stationery/content/stationery.jsm');
+Components.utils.import("resource://stationery/content/stationery.jsm");
+
 Stationery.initWindow(window);
